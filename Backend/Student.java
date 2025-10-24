8d536eeef5aea3626d5d21184c589b66f126e723
nader8757634 <naderking9968@gmail.com>
2025-10-25 01:58:44 +0300

    1221

--- Backend/Student.java
+++ Backend/Student.java
@@ -16,3 +16,54 @@ public class Student implements RecordsInterface {
         this.department = department;
         this.gpa = gpa;
     }
+
+	public String getStudentID() {
+		return studentID;
+	}
+
+	public void setStudentID(String studentID) {
+		this.studentID = studentID;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+
+	public double getGpa() {
+		return gpa;
+	}
+
+	public void setGpa(double gpa) {
+		this.gpa = gpa;
+	}
+    
+    
+}
