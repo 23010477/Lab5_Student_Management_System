c5b67b5f9be5f6ea6fafc49567d782884ae8a0aa
nader8757634 <naderking9968@gmail.com>
2025-10-25 01:55:30 +0300

    1

--- Backend/Student.java
+++ Backend/Student.java
@@ -8,5 +8,11 @@ public class Student implements RecordsInterface {
     private String department;
     private double gpa;
 
-   
+    public Student(String studentID, String name, int age, String gender, String department, double gpa) {
+        this.studentID = studentID;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.department = department;
+        this.gpa = gpa;
     }
